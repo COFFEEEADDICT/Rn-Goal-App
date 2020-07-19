2e25a1c90a215ccfcc222c3c433930a19a8b7f46
bryn <thenazboz@gmail.com>
2020-07-19 19:28:30 +0100

    goal input and add button working

--- App.js
+++ App.js
@@ -1,17 +1,48 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
+	const [enteredGoal, setEnteredGoal] = useState("");
+	const [courseGoals, setCourseGoals] = useState([]);
+
+	const goalInputHandler = (enteredText) => {
+		setEnteredGoal(enteredText);
+	};
+
+	const addGoalHandler = () => {
+		setCourseGoals([...courseGoals, enteredGoal]);
+	};
+
 	return (
-		<View>
-			<View>
-				<TextInput />
-				<Button title="ADD" />
+		<View style={styles.screen}>
+			<View style={styles.inputContainer}>
+				<TextInput
+					placeholder="Course Goal"
+					style={styles.input}
+					onChangeText={goalInputHandler}
+					value={enteredGoal}
+				/>
+				<Button title="ADD" onPress={addGoalHandler} />
 			</View>
 			<View></View>
 		</View>
 	);
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	screen: {
+		padding: 50,
+	},
+	inputContainer: {
+		alignItems: "center",
+		flexDirection: "row",
+		justifyContent: "space-between",
+	},
+	input: {
+		width: "80%",
+		borderBottomColor: "black",
+		borderWidth: 1,
+		padding: 10,
+	},
+});
